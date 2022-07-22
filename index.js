5e656ca6ec334de6f24387285520ea792c96f6db
Ranjit Kumar <ranjitkumar448@yahoo.com>
2022-07-22 16:07:04 +0530

    ip extracted

--- index.js
+++ index.js
@@ -21,7 +21,13 @@ app.get('/', function (req, res) {
 
 // your first API endpoint...
 app.get('/api/whoami', function (req, res) {
-  res.json({ ipaddress: req.ip, language: req.headers['accept-language'], software: req.headers['user-agent'] });
+  var software = req.headers['user-agent'].split(' ');
+  var data = {
+    ipaddress: req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.socket.remoteAddress || req.connection.socket.remoteAddress,
+    language: req.headers['accept-language'].split(',')[0],
+    software: software[1].slice(1) + " " + software[2] + " " + software[3].slice(0, -1)
+  }
+  res.json(data);
 });
 
 // listen for requests :)
